452cd3c8b0289f8eddc5a4e2938de5f7563ab753
gonzalomontenegro-cyber <gonzalo.montenegro@sellside.cl>
2025-09-22 10:54:59 -0300

    Correcion Boleta

--- simple_api/__manifest__.py
+++ simple_api/__manifest__.py
@@ -19,7 +19,7 @@
     'license': 'LGPL-3',
     'depends': ['base', 'account', 'contacts', 'mail','web'],
     'data': [
-        'views/boleta_honorarios_views.xml',
+        'views/boletas_honorarios_views.xml',
         'views/res_config_settings_views.xml',
         'data/ir_cron_data.xml',
         'security/ir.model.access.csv',
