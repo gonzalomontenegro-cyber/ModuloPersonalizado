5770b613fb98d48980c751ac3bacb5319c88c53c
gonzalomontenegro-cyber <gonzalo.montenegro@sellside.cl>
2025-09-23 19:12:28 +0000

    camio key api

--- simple_api/models/res_config_settings.py
+++ simple_api/models/res_config_settings.py
@@ -6,7 +6,7 @@ class ResConfigSettings(models.TransientModel):
     simpleapi_api_key = fields.Char(
         string='SimpleAPI Key',
         config_parameter='boleta_honorarios.simpleapi_api_key',
-        default='4648-N330-6392-2590-9354',
+        default='4284-W270-6392-7800-2954',
         help='API Key proporcionada por SimpleAPI'
     )
     
